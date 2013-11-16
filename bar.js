24650e5bddb7c42b01b869217716f76008f38370
Jóan Petur Petersen <joanpeturpetersen@gmail.com>
2013-11-16 14:57:59 +0100

    Labels added.

--- bar.js
+++ bar.js
@@ -1,7 +1,20 @@
 
+function BarChart(canvas, data) {
+    this.canvas = canvas;
+    this.data = data;
+    this.labels = [];
+    this.label_font = "14px Arial";
+    this.label_textAlign = "center";
+    this.label_textBaseLine = "bottom";
+    this.label_fillStyle = "#000";
+    this.label_padding = 10; // px
+}
 
+BarChart.prototype.set_labels = function set_labels(labels) {
+    this.labels = labels;
+}
 
-function draw_axis(canvas, padding, ticks) {
+BarChart.prototype.draw_axis = function draw_axis(canvas, padding, ticks) {
     y_tick_count = ticks.length;
     context = canvas.getContext('2d');
     context.beginPath();
@@ -18,7 +31,7 @@ function draw_axis(canvas, padding, ticks) {
 
     context.font = "13px Arial";
     context.textBaseline = 'middle';
-    context.fillStyle = "#999999";
+    context.fillStyle = "#000";
     
     tick_width = 4;
     var stepSize = (canvas.height-padding*2) / (y_tick_count - 1);
@@ -33,7 +46,8 @@ function draw_axis(canvas, padding, ticks) {
     context.stroke();
 }
 
-function draw_bars(canvas, data, dataplotmin, dataplotmax, padding) {
+BarChart.prototype.draw_bars = 
+  function draw_bars(canvas, data, dataplotmin, dataplotmax, padding) {
     // dataplotmax gives the highest data value on the plot (the top of
     // the y-axis).
     context = canvas.getContext('2d');
@@ -43,8 +57,8 @@ function draw_bars(canvas, data, dataplotmin, dataplotmax, padding) {
     bars_w = canvas.width - padding*2;
     bar_w = bars_w / M;
     bars_h = canvas.height - padding*2;
+    var h0 = (dataplotmax/(dataplotmax-dataplotmin)) * bars_h;
     for(var i=0; i<M; i++) {
-        var h0 = (dataplotmax/(dataplotmax-dataplotmin)) * bars_h;
         h = (data[i]/(dataplotmax-dataplotmin)) * bars_h;
         context.fillRect(padding + bar_w*i, h0+padding, bar_w, -h);
     }
@@ -53,9 +67,24 @@ function draw_bars(canvas, data, dataplotmin, dataplotmax, padding) {
     context.moveTo(padding, h0 + padding);
     context.lineTo(canvas.width-padding, h0 + padding);  
     context.stroke();
+    // Draw labels:
+    context.beginPath();
+    context.textAlign = this.label_textAlign;
+    context.textBaseline = this.label_textBaseline;
+    context.font = this.label_font;
+    context.fillStyle = this.label_fillStyle;
+    for(var i=0; i<this.labels.length; i++) {
+        var metrics = context.measureText(this.labels[i]);
+        console.log(metrics);
+        context.fillText(this.labels[i], padding + bar_w*i + bar_w/2,
+                h0 + padding - this.label_padding);
+    }              
+    context.stroke();
 }
 
-function plot_bar(canvas, data) {
+BarChart.prototype.draw = function draw() {
+    canvas = this.canvas;
+    data = this.data;
     datamax = Math.max.apply(Math, data);
     datamin = Math.min.apply(Math, data);
     datamin = Math.min(datamin, 0); // Should pref. start from 0
@@ -65,6 +94,6 @@ function plot_bar(canvas, data) {
     context.fillStyle = '#eee';
     context.fillRect(0, 0, canvas.width, canvas.height);
     var padding = 20;
-    draw_axis(canvas, padding, niceScale.getTicks());
-    draw_bars(canvas, data, niceScale.niceMin, niceScale.niceMax, padding);
+    this.draw_axis(canvas, padding, niceScale.getTicks());
+    this.draw_bars(canvas, data, niceScale.niceMin, niceScale.niceMax, padding);
 }
