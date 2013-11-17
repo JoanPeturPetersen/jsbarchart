f2a39ac0b03522597e1faaa15fada3e20449d55f
Jóan Petur Petersen <joanpeturpetersen@gmail.com>
2013-11-17 18:26:57 +0100

    Vertical labels.

--- bar.js
+++ bar.js
@@ -8,10 +8,24 @@ function BarChart(canvas, data) {
     this.label_textBaseLine = "bottom";
     this.label_fillStyle = "#000";
     this.label_padding = 10; // px
+
+    // Grid style:
+    this.grid_style = "rgba(150, 150, 150, 0.5)";
+
+    // Y label:
+    this.ylabel_angle = 0;
+    this.ylabel_textBaseline = 'middle';
+    this.ylabel_textAlign = 'center'; 
 }
 
 BarChart.prototype.set_labels = function set_labels(labels) {
     this.labels = labels;
+}
+
+BarChart.prototype.set_vertical_ylabels = function set_vertical_ylabels() {
+    this.ylabel_angle = -Math.PI / 2;
+    this.ylabel_textBaseline = 'top';
+    this.ylabel_textAlign = 'left';      
 }
 
 BarChart.prototype.draw_axis = function draw_axis(canvas, padding, ticks) {
@@ -28,6 +42,7 @@ BarChart.prototype.draw_axis = function draw_axis(canvas, padding, ticks) {
     var h0 = (dataplotmax/(dataplotmax-dataplotmin)) * bars_h; 
     context.moveTo(padding, h0 + padding);
     context.lineTo(canvas.width-padding, h0 + padding);
+    context.stroke();
 
     context.font = "13px Arial";
     context.textBaseline = 'middle';
@@ -37,20 +52,28 @@ BarChart.prototype.draw_axis = function draw_axis(canvas, padding, ticks) {
     var stepSize = (canvas.height-padding*2) / (y_tick_count - 1);
     for(var i=0; i<y_tick_count; i++) 
         if (parseFloat(ticks[y_tick_count - i - 1])!=0)
-        {
+        {   
+            context.beginPath();
             context.moveTo(padding - tick_width/2, padding + stepSize*i);
             context.lineTo(padding + tick_width/2, padding + stepSize*i);
-                context.fillText(ticks[y_tick_count - i - 1], 
-                        padding + tick_width/2, padding + stepSize*i);
+            context.stroke();
+            context.beginPath();
+            context.strokeStyle = this.grid_style;
+            context.moveTo(padding + tick_width/2, padding + stepSize*i);
+            context.lineTo(canvas.width - padding, padding + stepSize*i);
+            context.stroke();
+            context.beginPath();
+            context.fillText(ticks[y_tick_count - i - 1], 
+                padding + tick_width/2, padding + stepSize*i);
+            context.stroke();
         }
-    context.stroke();
 }
 
 BarChart.prototype.draw_ylabel = function draw_ylabel(canvas, text, padding) {
     context.save();
     context = canvas.getContext('2d');   
     context.translate(0, canvas.height/2);
-    context.rotate(-Math.PI / 2);
+    context.rotate(-Math.PI/2);
     context.textBaseline = 'top';
     context.textAlign = 'center';
     context.fillStyle = '#000';
@@ -67,7 +90,7 @@ BarChart.prototype.draw_bars =
     context.fillStyle = 'rgba(0, 0, 200, 0.5)';
     M = data.length;
     bars_w = canvas.width - padding*2;
-    bar_w = bars_w / M;
+    bar_w = Math.ceil(bars_w / M);
     bars_h = canvas.height - padding*2;
     var h0 = (dataplotmax/(dataplotmax-dataplotmin)) * bars_h;
     for(var i=0; i<M; i++) {
@@ -80,18 +103,22 @@ BarChart.prototype.draw_bars =
     context.lineTo(canvas.width-padding, h0 + padding);  
     context.stroke();
     // Draw labels:
-    context.beginPath();
     context.textAlign = this.label_textAlign;
     context.textBaseline = this.label_textBaseline;
     context.font = this.label_font;
     context.fillStyle = this.label_fillStyle;
     for(var i=0; i<this.labels.length; i++) {
-        var metrics = context.measureText(this.labels[i]);
-        console.log(metrics);
-        context.fillText(this.labels[i], padding + bar_w*i + bar_w/2,
+        context.beginPath();
+        context.save();
+        context.textAlign = this.ylabel_textAlign;
+        context.textBaseline = this.ylabel_textBaseline;
+        context.translate(padding + bar_w*i + bar_w/2,
                 h0 + padding - this.label_padding);
+        context.rotate(this.ylabel_angle); // -Math.PI/2); 
+        context.fillText(this.labels[i], 0, 0);
+        context.restore();
+        context.stroke();
     }              
-    context.stroke();
 }
 
 BarChart.prototype.draw = function draw() {
